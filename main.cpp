f5b472dbd0d34232c8f7dfbf3b5b0726ae5229f5
Vlad <104225311+senyyoor@users.noreply.github.com>
2024-04-09 18:27:53 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  int y = 6;
+  int sum = x + y;
+  cout << sum;
+  return 0;
+}
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x;
+  cout << "Type a number: "; // Type a number and press enter
+  cin >> x;                  // Get user input from the keyboard
+  cout << "Your number is: " << x;
+  return 0;
+}
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x, y;
+  int sum;
+  cout << "Type a number: ";
+  cin >> x;
+  cout << "Type another number: ";
+  cin >> y;
+  sum = x + y;
+  cout << "Sum is: " << sum;
+  return 0;
+}
+
+
